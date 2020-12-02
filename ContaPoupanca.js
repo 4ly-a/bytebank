41aaeeb93857f44dfa6957d2e210c5bee2bd6848
4ly <alyssonalcantaralves@gmail.com>
2020-12-02 09:49:47 -0300

    Organizando

--- ContaPoupanca.js
+++
@@ -1,7 +0,0 @@
-import Conta from "./Conta.js";
-
-export default class ContaPoupanca extends Conta {
-  constructor(saldoInicial, cliente, agencia) {
-    super(saldoInicial, cliente, agencia);
-  }
-}
